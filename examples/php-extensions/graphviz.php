e4463b5e2ed7ee81cfda327565c1b495aaa1308c
Laurent Laville <laurent.laville@gmail.com>
2020-04-27 09:00:48 +0000

    use levels parameter of dirname()

--- examples/php-extensions/graphviz.php
+++ examples/php-extensions/graphviz.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
+require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
 
 use Bartlett\GraphUml\ClassDiagramBuilder;
 use Bartlett\GraphUml\Generator\GraphVizGenerator;
