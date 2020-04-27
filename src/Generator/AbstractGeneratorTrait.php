b32e754499ecbf81bdf194b3b1e1862a45097f8d
Laurent Laville <laurent.laville@gmail.com>
2020-04-27 09:09:47 +0000

    fix FormatterInterface namespace

--- src/Generator/AbstractGeneratorTrait.php
+++ src/Generator/AbstractGeneratorTrait.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Bartlett\GraphUml\Generator;
 
-use Bartlett\GraphUml\FormatterInterface;
+use Bartlett\GraphUml\Formatter\FormatterInterface;
 
 use ReflectionClass;
 use ReflectionExtension;
